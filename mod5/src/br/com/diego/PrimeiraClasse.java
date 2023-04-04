c9e6e32fb8b3e7e45faf22d0a51cada77a93d593
DiegoTeixeiraPro <diegoateixeira.pro@gmail.com>
2023-04-04 17:43:56 -0300

    mod5

---
+++ mod5/src/br/com/diego/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+package br.com.diego;
+
+public class PrimeiraClasse {
+	public static void main(String[] args) {
+		System.out.print("Hello Diego!!!");
+	}
+}
---
+++ mod5/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author diego
+ *
+ */
+module PrimeiroProjeto {
+}
